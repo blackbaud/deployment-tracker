a305e29dc2165a1a032d45f9f0128a47212c91d1
Eric Slater <eric.slater@blackbaud.com>
2016-07-20 08:43:33 -0500

    LUM-10149 Deployment tracker shows list of artifacts (#38)

--- src/main/java/com/blackbaud/deployment/core/domain/ArtifactInfoRepository.java
+++ src/main/java/com/blackbaud/deployment/core/domain/ArtifactInfoRepository.java
@@ -1,12 +1,12 @@
 package com.blackbaud.deployment.core.domain;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ArtifactInfoRepository extends CrudRepository<ArtifactInfoEntity, ArtifactInfoPrimaryKey> {
 
+    List<ArtifactInfoEntity> findByArtifactId(String artifactId);
     ArtifactInfoEntity findFirstByArtifactIdAndBuildVersionLessThanOrderByBuildVersionDesc(String artifactId, String buildVersion);
     List<ArtifactInfoEntity> findByArtifactIdAndBuildVersionLessThanEqual(String artifactId, String toVersion);
     List<ArtifactInfoEntity> findByArtifactIdAndBuildVersionGreaterThan(String artifactId, String fromVersion);
--- src/main/java/com/blackbaud/deployment/resources/ArtifactInfoResource.java
+++ src/main/java/com/blackbaud/deployment/resources/ArtifactInfoResource.java
@@ -19,6 +19,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Component
 @Path(ResourcePaths.ARTIFACT_INFO_PATH)
@@ -40,6 +41,16 @@ public class ArtifactInfoResource {
     public ArtifactInfo put(@PathParam("artifactId") String artifactId, @PathParam("buildVersion") String buildVersion,
                             @Valid ArtifactInfo artifactInfo) {
          return artifactInfoService.create(artifactId, buildVersion, converter.toEntity(artifactInfo));
+    }
+
+    @GET
+    @Path("{artifactId}")
+    public List<ArtifactInfo> find(@PathParam("artifactId") String artifactId) {
+        List<ArtifactInfoEntity> artifactInfoList = artifactInfoRepository.findByArtifactId(artifactId);
+        if (artifactInfoList == null) {
+            throw new NotFoundException();
+        }
+        return converter.toApiList(artifactInfoList);
     }
 
     @GET
