6e7a377a0b9af91d393142573a9056d664bf5f7e
Eric Slater <eric.slater@blackbaud.com>
2016-07-26 12:04:45 -0500

    specify artifact id when fetching git log (#42)

--- src/main/java/com/blackbaud/deployment/core/domain/GitLogRepository.java
+++ src/main/java/com/blackbaud/deployment/core/domain/GitLogRepository.java
@@ -8,11 +8,11 @@ import java.util.List;
 
 public interface GitLogRepository extends CrudRepository<GitLogEntity, GitLogPrimaryKey> {
 
-    @Query(value = "select * from git_log where ordinal <= (select ordinal from git_log where git_sha = :currentSha) and ordinal > (select ordinal from git_log where git_sha = :previousSha)", nativeQuery = true)
-    List<GitLogEntity> fetchGitLogForCurrentAndPreviousGitShas(@Param("currentSha") String currentSha, @Param("previousSha") String previousSha);
+    @Query(value = "select * from git_log where ordinal <= (select ordinal from git_log where git_sha = :currentSha and artifact_id = :artifactId) and ordinal > (select ordinal from git_log where git_sha = :previousSha and artifact_id = :artifactId)", nativeQuery = true)
+    List<GitLogEntity> fetchGitLogForCurrentAndPreviousGitShas(@Param("artifactId") String artifactId, @Param("currentSha") String currentSha, @Param("previousSha") String previousSha);
 
-    @Query(value = "select * from git_log where ordinal <= (select ordinal from git_log where git_sha = :sha)", nativeQuery = true)
-    List<GitLogEntity> fetchGitLogUntilSha(@Param("sha") String sha);
+    @Query(value = "select * from git_log where ordinal <= (select ordinal from git_log where git_sha = :sha and artifact_id = :artifactId)", nativeQuery = true)
+    List<GitLogEntity> fetchGitLogUntilSha(@Param("artifactId") String artifactId, @Param("sha") String sha);
 
     @Query(value = "select * from git_log where artifact_id = :artifactId order by ordinal asc", nativeQuery = true)
     List<GitLogEntity> fetchOrderedGitLogForArtifactId(@Param("artifactId") String artifactId);
