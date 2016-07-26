04e5fd920398e07ff550bf0c32421690d70c272c
Blackbaud-EricSlater <eric.slater@blackbaud.com>
2016-07-26 16:06:32 -0500

    LUM-10375 add test asserting log can be retrieved when entries are interspersed with other artifacts

--- src/componentTest/groovy/com/blackbaud/deployment/core/domain/GitLogRepositorySpec.groovy
+++ src/componentTest/groovy/com/blackbaud/deployment/core/domain/GitLogRepositorySpec.groovy
@@ -1,6 +1,10 @@
 package com.blackbaud.deployment.core.domain
 
+import com.blackbaud.deployment.ArtifactInfoConverter
 import com.blackbaud.deployment.ComponentTest
+import com.blackbaud.deployment.RealArtifacts
+import com.blackbaud.deployment.api.ArtifactInfo
+import com.blackbaud.deployment.client.ArtifactInfoClient
 import org.springframework.beans.factory.annotation.Autowired
 import spock.lang.Specification
 
@@ -9,6 +13,16 @@ class GitLogRepositorySpec extends Specification {
 
     @Autowired
     GitLogRepository gitLogRepository
+
+    @Autowired
+    ArtifactInfoConverter artifactInfoConverter
+
+    @Autowired
+    ArtifactInfoClient artifactInfoClient
+
+    private final ArtifactInfo earlyDeploymentTrackerArtifact = RealArtifacts.getEarlyDeploymentTrackerArtifact()
+    private final ArtifactInfo recentDeploymentTrackerArtifact = RealArtifacts.getRecentDeploymentTrackerArtifact()
+    private final ArtifactInfo bluemoonCoreArtifact = RealArtifacts.getBluemoonCoreArtifact()
 
     def "i can retrieve commits in the order they were commited"() {
         given:
@@ -25,4 +39,16 @@ class GitLogRepositorySpec extends Specification {
         retrieved == entities
     }
 
+    def "the git log returns correct results when entries are interspersed"() {
+        given:
+        artifactInfoClient.update("deployment-tracker", earlyDeploymentTrackerArtifact.buildVersion, earlyDeploymentTrackerArtifact)
+        artifactInfoClient.update("bluemoon-core", bluemoonCoreArtifact.buildVersion, bluemoonCoreArtifact)
+        artifactInfoClient.update("deployment-tracker", recentDeploymentTrackerArtifact.buildVersion, recentDeploymentTrackerArtifact)
+
+        when:
+        List<GitLogEntity> retrieved = gitLogRepository.fetchGitLogUntilSha("deployment-tracker", recentDeploymentTrackerArtifact.gitSha)
+
+        then:
+        retrieved.size() == 22
+    }
 }
