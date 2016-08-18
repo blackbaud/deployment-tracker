c0133b68b841943036fa8a2a51a533a0ef77cb2f
Ryan McKay <ryan.mckay@blackbaud.com>
2016-08-18 18:17:49 -0500

    LUM-11446 Random builders match schema

--- src/mainTest/groovy/com/blackbaud/deployment/api/RandomArtifactReleaseInfoBuilder.groovy
+++ src/mainTest/groovy/com/blackbaud/deployment/api/RandomArtifactReleaseInfoBuilder.groovy
@@ -5,10 +5,10 @@ import static com.blackbaud.deployment.api.ClientARandom.aRandom
 class RandomArtifactReleaseInfoBuilder extends ArtifactReleaseInfo.ArtifactReleaseInfoBuilder {
 
     public RandomArtifactReleaseInfoBuilder() {
-        artifactId(aRandom.text(20))
-        buildVersion(aRandom.text(20))
-        releaseVersion(aRandom.text(20))
-        gitSha(aRandom.text(20))
+        artifactId(aRandom.text(100))
+        buildVersion(aRandom.text(100))
+        releaseVersion(aRandom.text(100))
+        gitSha(aRandom.text(100))
     }
 
 }
--- src/mainTest/groovy/com/blackbaud/deployment/api/RandomArtifactReleaseLogBuilder.groovy
+++ src/mainTest/groovy/com/blackbaud/deployment/api/RandomArtifactReleaseLogBuilder.groovy
@@ -5,12 +5,12 @@ import static com.blackbaud.deployment.api.ClientARandom.aRandom
 class RandomArtifactReleaseLogBuilder extends ArtifactReleaseInfo.ArtifactReleaseInfoBuilder {
 
     public RandomArtifactReleaseLogBuilder() {
-        artifactId(aRandom.text(20))
-                .buildVersion(aRandom.text(20))
-                .releaseVersion(aRandom.text(20))
-                .prevBuildVersion(aRandom.text(20))
-                .prevReleaseVersion(aRandom.text(20))
-                .deployer(aRandom.text(20))
+        artifactId(aRandom.text(100))
+                .buildVersion(aRandom.text(100))
+                .releaseVersion(aRandom.text(100))
+                .prevBuildVersion(aRandom.text(100))
+                .prevReleaseVersion(aRandom.text(100))
+                .deployer(aRandom.text(100))
                 .releaseDate(aRandom.zonedDateTime())
     }
 }
