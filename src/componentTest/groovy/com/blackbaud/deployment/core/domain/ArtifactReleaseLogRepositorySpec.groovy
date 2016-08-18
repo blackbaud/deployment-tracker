c31bd6d8fa5adf5b18bbe790623dd62a382c01a0
Blackbaud-AlexFortin <alex.fortin@blackbaud.com>
2016-08-18 14:21:26 -0500

    LUM-11446 Modified test

--- src/componentTest/groovy/com/blackbaud/deployment/core/domain/ArtifactReleaseLogRepositorySpec.groovy
+++ src/componentTest/groovy/com/blackbaud/deployment/core/domain/ArtifactReleaseLogRepositorySpec.groovy
@@ -1,11 +1,14 @@
 package com.blackbaud.deployment.core.domain
 
+import com.blackbaud.deployment.ArtifactInfoConverter
 import com.blackbaud.deployment.ComponentTest
 import com.blackbaud.deployment.RealArtifacts
 import com.blackbaud.deployment.api.ArtifactInfo
 import com.blackbaud.deployment.api.ArtifactReleaseLog
 import com.blackbaud.deployment.client.ArtifactInfoClient
 import com.blackbaud.deployment.client.ArtifactReleaseLogClient
+import com.blackbaud.deployment.client.GitLogInfoClient
+import org.junit.BeforeClass
 import org.springframework.beans.factory.annotation.Autowired
 import spock.lang.Specification
 
@@ -21,11 +24,31 @@ class ArtifactReleaseLogRepositorySpec extends Specification {
     ArtifactReleaseLogClient artifactReleaseLogClient;
 
     @Autowired
-    private ArtifactInfoClient artifactInfoClient
+    ArtifactInfoClient artifactInfoClient
+
+    @Autowired
+    GitLogRepository gitLogRepository;
+
+    @Autowired
+    GitLogInfoClient gitLogInfoClient;
+
+    @Autowired
+    ArtifactInfoRepository artifactInfoRepository;
+
+    @Autowired
+    ArtifactInfoConverter converter;
+
+    private final ArtifactInfo deploymentTrackerArtifact = RealArtifacts.getEarlyDeploymentTrackerArtifact()
 
     private ArtifactInfo earlyInfo = RealArtifacts.getEarlyDeploymentTrackerArtifact()
     private ArtifactInfo middleInfo = RealArtifacts.getMiddleDeploymentTrackerArtifact()
-    private ArtifactInfo recentInfo = RealArtifacts.getRecentDeploymentTrackerArtifact()
+
+    @BeforeClass
+    void setUpOnce() {
+//        artifactInfoRepository.save(converter.toEntity(deploymentTrackerArtifact));
+//        gitLogInfoClient.post(deploymentTrackerArtifact.artifactId)
+//        gitLogRepository.fetchOrderedGitLogForArtifactId(deploymentTrackerArtifact.artifactId)
+    }
 
     def "test" () {
         given:
@@ -47,19 +70,30 @@ class ArtifactReleaseLogRepositorySpec extends Specification {
 
     def "Should be able do get a list of artifact logs" () {
         given:
-        ArtifactReleaseInfoLogEntity entry1 = aRandom.releaseLogEntity().artifactId(earlyInfo.artifactId).buildVersion(earlyInfo.buildVersion).prevBuildVersion(middleInfo.buildVersion).build()
-        ArtifactReleaseInfoLogEntity entry2 = aRandom.releaseLogEntity().artifactId(middleInfo.artifactId).buildVersion(middleInfo.buildVersion).prevBuildVersion(recentInfo.buildVersion).build()
+        ArtifactReleaseInfoLogEntity logEntity = aRandom.releaseLogEntity().artifactId(middleInfo.artifactId).buildVersion(middleInfo.buildVersion).prevBuildVersion(earlyInfo.buildVersion).build()
         artifactInfoClient.update(earlyInfo.artifactId, earlyInfo.buildVersion, earlyInfo)
         artifactInfoClient.update(middleInfo.artifactId, middleInfo.buildVersion, middleInfo)
-        artifactInfoClient.update(recentInfo.artifactId, recentInfo.buildVersion, recentInfo)
+        artifactInfoRepository.save(converter.toEntity(deploymentTrackerArtifact));
+        gitLogInfoClient.post(deploymentTrackerArtifact.artifactId)
+        gitLogRepository.fetchOrderedGitLogForArtifactId(deploymentTrackerArtifact.artifactId)
 
         when:
-        artifactReleaseLogRepository.save(entry1)
-        artifactReleaseLogRepository.save(entry2)
+        artifactReleaseLogRepository.save(logEntity)
 
+        and:
+        ArtifactReleaseLog expected = ArtifactReleaseLog.builder()
+                .artifactId(logEntity.artifactId)
+                .buildVersion(logEntity.buildVersion)
+                .prevBuildVersion(logEntity.prevBuildVersion)
+                .releaseVersion(logEntity.releaseVersion)
+                .prevReleaseVersion(logEntity.prevReleaseVersion)
+                .deployer(logEntity.deployer)
+                .stories(["LUM-8045", "LUM-7759"] as Set)
+                .developers(["Ryan McKay"] as Set)
+                .build();
         then:
-        List<ArtifactReleaseLog> expected = artifactReleaseLogClient.findAll()
-        assert expected != null
+        List<ArtifactReleaseLog> artifactReleaseLogList = artifactReleaseLogClient.findAll()
+        assert expected in artifactReleaseLogList
 
     }
 }
