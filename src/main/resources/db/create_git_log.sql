00deabb4097c8538c14feeda2d2c10f92521d753
Blackbaud-EricSlater <eric.slater@blackbaud.com>
2016-08-26 16:24:55 -0500

    LUM-11446 add index for artifact_id and ordinal on git log. change query to lookup using primary key so index is used.

--- src/main/resources/db/create_git_log.sql
+++ src/main/resources/db/create_git_log.sql
@@ -38,3 +38,34 @@ END;
 $$ language plpgsql;
 #
 --rollback drop function fetch_git_log(varchar,varchar,varchar)
+
+--changeset blackbaud:3
+create unique index git_log_artifact_id_ordinal_idx on git_log(artifact_id, ordinal);
+--rollback drop index if exists git_log_artifact_id_ordinal_idx;
+
+--changeset blackbaud:4 endDelimiter:#
+#
+CREATE OR REPLACE FUNCTION fetch_git_log(artifactId varchar, curBuildVersion varchar, prevBuildVersion varchar) RETURNS setof git_log as $$
+DECLARE
+  curVersionOrdinal integer;
+  prevVersionOrdinal integer;
+BEGIN
+  begin
+    select into strict curVersionOrdinal ordinal from git_log where artifact_id = artifactId and git_sha = (select git_sha from artifact_info where artifact_id = artifactId and build_Version = curBuildVersion);
+  exception
+      when no_data_found then
+        raise notice 'current version has no associated git sha!!!';
+  end;
+
+  begin
+    select into strict prevVersionOrdinal ordinal from git_log where artifact_id = artifactId and git_sha = (select git_sha from artifact_info where artifact_id = artifactId and build_Version = prevBuildVersion);
+  exception
+      when no_data_found then
+        prevVersionOrdinal := 0;
+  end;
+
+  return query select * from git_log where ordinal <= curVersionOrdinal and ordinal > prevVersionOrdinal and artifact_id = artifactId;
+END;
+$$ language plpgsql;
+#
+--rollback drop function fetch_git_log(varchar,varchar,varchar)
