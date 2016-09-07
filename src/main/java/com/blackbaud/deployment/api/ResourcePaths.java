f186a60e6e694e6e01466d0e1571946a1f525675
Blackbaud-ChrisMecklenborg <christopher.mecklenborg@blackbaud.com>
2016-09-07 13:53:35 -0500

    LUM-11018 Corrected reorder resource path

--- src/main/java/com/blackbaud/deployment/api/ResourcePaths.java
+++ src/main/java/com/blackbaud/deployment/api/ResourcePaths.java
@@ -14,6 +14,7 @@ public class ResourcePaths {
     public static final String ARTIFACT_PATH = "artifacts";
     public static final String RELEASE_PLAN_DIFF_PATH = CURRENT_PATH + "/release-plan-diff";
     public static final String ARTIFACT_RELEASE_REPORT = DEPLOYMENT_TRACKER_PATH + "/artifact-release-report";
+    public static final String RELEASE_PLAN_ARTIFACT_REORDER = DEPLOYMENT_TRACKER_PATH + "/release-plan-artifact-reorder";
     public static final String REMEDIATE_PATH = "remediate";
     public static final String RELEASE_PLAN_ARTIFACT_REORDER = DEPLOYMENT_TRACKER_PATH + "/release-plan-artifact-reorder";
 }
