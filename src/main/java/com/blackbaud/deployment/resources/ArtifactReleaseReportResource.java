7b47981296973925555a1f9465aa2139c73a98fc
Blackbaud-DiHuynh <di.huynh@blackbaud.com>
2016-10-12 16:28:31 -0500

    Lum-13439 make artifact report respect foundation (#92)

--- src/main/java/com/blackbaud/deployment/resources/ArtifactReleaseReportResource.java
+++ src/main/java/com/blackbaud/deployment/resources/ArtifactReleaseReportResource.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -25,8 +26,8 @@ public class ArtifactReleaseReportResource {
     private ArtifactReleaseDiffConverter converter;
 
     @GET
-    public List<ArtifactReleaseDiff> getArtifactReleaseLog() {
-        return converter.toApiList(artifactReleaseLogReportQuery.getArtifactReleaseReport());
+    @Path("{foundation}")
+    public List<ArtifactReleaseDiff> getArtifactReleaseLog(@PathParam("foundation") String foundation) {
+        return converter.toApiList(artifactReleaseLogReportQuery.getArtifactReleaseReport(foundation));
     }
 }
-
