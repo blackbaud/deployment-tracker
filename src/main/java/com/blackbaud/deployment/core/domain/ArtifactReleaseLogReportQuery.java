4574f4e4c3296467b85fea5fd7d182b8c26e85a8
Blackbaud-DiHuynh <di.huynh@blackbaud.com>
2016-10-14 10:15:26 -0500

    Revert "Lum-13439 added back old resource for backwards compat" (#94)

--- src/main/java/com/blackbaud/deployment/core/domain/ArtifactReleaseLogReportQuery.java
+++ src/main/java/com/blackbaud/deployment/core/domain/ArtifactReleaseLogReportQuery.java
@@ -16,21 +16,6 @@ public class ArtifactReleaseLogReportQuery {
     @Inject
     private EntityManager entityManager;
 
-    public List<ArtifactReleaseLogReportResult> getArtifactReleaseReport() {
-        Query query = entityManager.createNativeQuery(
-                "select arl.*, string_agg(distinct gl.story_id\\:\\:text, ',') as stories, " +
-                        "string_agg(distinct gl.author\\:\\:text, ',') as developers, " +
-                        "(select git_sha from artifact_info where build_version = arl.build_version and artifact_id = arl.artifact_id) as git_sha, " +
-                        "(select git_sha from artifact_info where build_version = arl.prev_build_version and artifact_id = arl.artifact_id) as prev_git_sha " +
-                        "from artifact_release_log arl left join " +
-                        "fetch_git_log(arl.artifact_id, arl.build_version, arl.prev_build_version) gl " +
-                        "on arl.artifact_id = gl.artifact_id " +
-                        "group by arl.artifact_id, arl.foundation, arl.space, arl.deployer, arl.release_version, arl.build_version, arl.prev_release_version, arl.prev_build_version " +
-                        "order by release_version desc;",
-                ArtifactReleaseLogReportResult.class);
-        return query.getResultList();
-    }
-
     public List<ArtifactReleaseLogReportResult> getArtifactReleaseReport(String foundation) {
         Query query = entityManager.createNativeQuery(
                 "select arl.*, string_agg(distinct gl.story_id\\:\\:text, ',') as stories, " +
